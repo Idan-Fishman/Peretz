621603df7b5902f5157a187d78008d98b1a59126
Idan Fishman <ifishman.swe@gmail.com>
2024-05-07 21:45:49 +0300

    feat: setup initial vite auth (#1)

---
+++ web/src/components/auth/LoginForm.jsx
@@ -0,0 +1,123 @@
+import { CssVarsProvider } from "@mui/joy/styles";
+import GlobalStyles from "@mui/joy/GlobalStyles";
+import CssBaseline from "@mui/joy/CssBaseline";
+import Box from "@mui/joy/Box";
+import Button from "@mui/joy/Button";
+import Divider from "@mui/joy/Divider";
+import FormControl from "@mui/joy/FormControl";
+import FormLabel from "@mui/joy/FormLabel";
+import Link from "@mui/joy/Link";
+import Input from "@mui/joy/Input";
+import Typography from "@mui/joy/Typography";
+import Stack from "@mui/joy/Stack";
+import { GoogleIcon } from "../icons/GoogleIcon";
+
+export default function LoginForm() {
+  return (
+    <CssVarsProvider defaultMode="dark" disableTransitionOnChange>
+      <CssBaseline />
+      <GlobalStyles
+        styles={{
+          ":root": {
+            "--Form-maxWidth": "800px",
+            "--Transition-duration": "0.4s", // set to `none` to disable transition
+          },
+        }}
+      />
+
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "100dvh",
+          width: "100%",
+          px: 2,
+        }}
+      >
+        <Box
+          component="main"
+          sx={{
+            my: "auto",
+            py: 2,
+            pb: 5,
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
+            width: 400,
+            maxWidth: "100%",
+            mx: "auto",
+            borderRadius: "sm",
+            "& form": {
+              display: "flex",
+              flexDirection: "column",
+              gap: 2,
+            },
+            [`& .MuiFormLabel-asterisk`]: {
+              visibility: "hidden",
+            },
+          }}
+        >
+          <Stack gap={4} sx={{ mb: 2 }}>
+            <Stack gap={1}>
+              <Typography component="h1" level="h3">
+                Sign in
+              </Typography>
+              <Typography level="body-sm">
+                New to company?{" "}
+                <Link href="#replace-with-a-link" level="title-sm">
+                  Sign up!
+                </Link>
+              </Typography>
+            </Stack>
+            <Button
+              variant="soft"
+              color="neutral"
+              fullWidth
+              startDecorator={<GoogleIcon />}
+            >
+              Continue with Google
+            </Button>
+          </Stack>
+          <Divider
+            sx={(theme) => ({
+              [theme.getColorSchemeSelector("light")]: {
+                color: { xs: "#FFF", md: "text.tertiary" },
+              },
+            })}
+          >
+            or
+          </Divider>
+          <Stack gap={4} sx={{ mt: 2 }}>
+            <form
+              onSubmit={(event) => {
+                event.preventDefault();
+                const formElements = event.currentTarget.elements;
+                const data = {
+                  email: formElements.email.value,
+                  password: formElements.password.value,
+                };
+                alert(JSON.stringify(data, null, 2));
+              }}
+            >
+              <FormControl required>
+                <FormLabel>Email</FormLabel>
+                <Input type="email" name="email" />
+              </FormControl>
+              <FormControl required>
+                <FormLabel>Password</FormLabel>
+                <Input type="password" name="password" />
+              </FormControl>
+              <Stack gap={4} sx={{ mt: 2 }}>
+                <Button type="submit" fullWidth>
+                  Sign in
+                </Button>
+              </Stack>
+            </form>
+          </Stack>
+        </Box>
+      </Box>
+    </CssVarsProvider>
+  );
+}
+
+export { LoginForm };
